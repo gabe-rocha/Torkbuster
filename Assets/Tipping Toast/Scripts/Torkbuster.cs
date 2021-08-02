a9302a2719618f56c4bab1c6634650651592f1f8
Gabe Rocha <gabrielcco@gmail.com>
2021-08-02 14:14:59 -0500

    Initial commit

---
+++ Assets/Tipping Toast/Scripts/Torkbuster.cs
@@ -0,0 +1,17 @@
+using System.Transactions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torkbuster : MonoBehaviour
+{
+
+    public int rpm = 100;
+
+    private void Update()
+    {
+        Vector3 rotation = new Vector3(-rpm * 6f * Time.deltaTime, 0f, 0f);
+        transform.Rotate(rotation);
+
+    }
+}
