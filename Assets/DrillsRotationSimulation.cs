d6d92f9d622a6baa68829cd7abe670523fe63dc1
Gabe Rocha <gabrielcco@gmail.com>
2021-08-07 09:36:52 -0500

    Final Version

--- Assets/DrillsRotationSimulation.cs
+++ Assets/DrillsRotationSimulation.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Contracts;
 using System.Transactions;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,10 +10,12 @@ public class DrillsRotationSimulation : MonoBehaviour
 {
     public float currentRPM = 50;
     public float rpmIncreaseStep = 10f;
-    public float rpmIncreaseWithShock = 50f;
+    public float rpmChangeSpeed = 2f;
+    public float torqueIncreaseWithShock = 50f;
     public float minRPM = 0f;
     public float maxRPM = 200f;
-    public float rpmReductionDuration = 3f;
+    public float torqueReductionDuration = 3f;
+    public float rpmToTorqueFactor = 1.5f;
     public float vibrationFrequency = 0.1f;
     public float vibrationAmplitude = 0.0025f;
     public float vibrationShockFactor = 2f;
@@ -22,22 +25,27 @@ public class DrillsRotationSimulation : MonoBehaviour
     public float stutteringFactor = 2f;
     public float torkbusterVibeAndFreqReductionFactor = 0.5f;
     public Transform torkBuster;
-    public ParticleSystem particlesBlue, particlesRed;
     public TextMeshProUGUI textRPM, textStutter;
     public Color stutterColor;
     public Transform drillToBeReplaced;
-    public Slider rpmSlider;
-    public Transform rpmGaugeArrow;
-
+    public Image torqueGauge;
+    public Transform torqueGaugeArrow;
+    public Button rpmUp, rpmDown;
+    public Light sceneLight;
+    public Gradient minMaxGaugeColor;
+    public Image shockFeedbackImageBad, shockFeedbackImageGood;
+    public float shockFeedbackDuration = 0.2f;
+    public GameObjectPooler redParticlesPool, blueParticlesPool;
+    
 
     private bool isTorkbusterInstalled = false;
     private int indexDrillReplacement = 2;
 
     private bool isStopped = false;
     private float zRotationEulersPerSecond = 0;
-    private Coroutine shockCoRo, coroRemovingOldDrillOut, coroInstallingOldDrill, coroInstallingTorkBuster, coroRemovingTorkBuster;
+    private Coroutine shockCoRo, coroRemovingOldDrillOut, coroInstallingOldDrill, coroInstallingTorkBuster, coroRemovingTorkBuster, changeRPMCoro, badShockFeedback, goodShockFeedback;
     private bool isShocking = false;
-    private float oldRPM;
+    private float oldTorque;
     // private Vector3 drillInitialPos;
     private bool isDrillMovingUp = true;
     private Vector3 targetPosUp, targetPosDown;
@@ -45,54 +53,70 @@ public class DrillsRotationSimulation : MonoBehaviour
     private bool isStuttering = false;
     private float stutterStartTime, lastStutterDraw;
     private float maxFreq, maxAmp;
-    private float rpmToGaugeArrowfactor;
+    private float rpmToGaugeArrowRotationfactor, currentTorque;    
+    private float targetRPM;    
 
 
     private void Start()
     {
-        zRotationEulersPerSecond = 360f * currentRPM / 60f;
+        
         targetPosUp = Vector3.up * vibrationAmplitude;
         targetPosDown = -1 * Vector3.up * vibrationAmplitude;
-        oldRPM = currentRPM;
+        
+        currentTorque = currentRPM * rpmToTorqueFactor;
+        zRotationEulersPerSecond = 360f * currentRPM / 60f;
+
+        oldTorque = currentTorque;
+        targetRPM = currentRPM;
+
         defaultVibrationFrequency = vibrationFrequency;
         defaultVibrationAmplitude = vibrationAmplitude;
 
         maxFreq = defaultVibrationFrequency * 3f;
         maxAmp = defaultVibrationAmplitude * 3f;
 
-        rpmSlider.value = currentRPM;
-        rpmSlider.onValueChanged.AddListener (delegate {RPMSliderValueChanged ();});
+        // rpmSlider.value = currentRPM;
+        // rpmSlider.onValueChanged.AddListener (delegate {RPMSliderValueChanged ();});
         
-        rpmToGaugeArrowfactor = -144.5f / maxRPM; //200 rpm is 144.5 degrees
+        rpmToGaugeArrowRotationfactor = -144.5f / maxRPM; //200 rpm is 144.5 degrees
+        shockFeedbackImageBad.enabled = false;
+        shockFeedbackImageGood.enabled = false;
+        shockFeedbackImageBad.CrossFadeAlpha(0,0, true);
+        shockFeedbackImageGood.CrossFadeAlpha(0,0, true);
+
 
         UpdateUI();
     }
 
-    public void RPMSliderValueChanged()
-	{
-        if(isShocking){
-            return;
-        }
-		currentRPM = rpmSlider.value;
-        oldRPM = currentRPM;
-        zRotationEulersPerSecond = 360f * currentRPM / 60f;
-        UpdateUI();
-	}
+    // public void RPMSliderValueChanged()
+	// {
+    //     if(isShocking){
+    //         return;
+    //     }
+	// 	currentRPM = rpmSlider.value;
+    //     oldRPM = currentRPM;
+    //     zRotationEulersPerSecond = 360f * currentRPM / 60f;
+    //     UpdateUI();
+	// }
 
     private void Update()
     {
         RotateDrillsContinuously();
         VibrateDrillsContinuously();
         StutterDrills();
-        AnimateRPMGaugeArrow();
+        UpdateTorqueGauge();
+        // UpdateLight();
     }
 
-
+    private void UpdateLight()
+    {
+        sceneLight.color = minMaxGaugeColor.Evaluate(currentRPM / maxRPM);
+    }
 
     private void RotateDrillsContinuously()
     {
-        //transform.Rotate(Vector3.forward * zRotationEulersPerSecond * Time.deltaTime, Space.Self);
-        torkBuster.Rotate(-1f * Vector3.right * zRotationEulersPerSecond * Time.deltaTime, Space.Self);
+        transform.Rotate(Vector3.forward * zRotationEulersPerSecond * Time.deltaTime * -1, Space.Self);
+        // torkBuster.Rotate(-1f * Vector3.right * zRotationEulersPerSecond * Time.deltaTime, Space.Self);
     }
 
     private void VibrateDrillsContinuously(){
@@ -129,15 +153,15 @@ public class DrillsRotationSimulation : MonoBehaviour
         }
 
         //Calculate target positions
-        targetPosUp = Vector3.up * vibrationAmplitude * currentRPM;
-        targetPosDown = -1 * Vector3.up * vibrationAmplitude * currentRPM;
+        targetPosUp = Vector3.up * vibrationAmplitude * targetRPM;
+        targetPosDown = -1 * Vector3.up * vibrationAmplitude * targetRPM;
 
         if(isDrillMovingUp)
         {
             if(transform.position.y < targetPosUp.y){
                 targetPosUp.x = transform.position.x;
                 targetPosUp.z = transform.position.z;
-                transform.position = Vector3.MoveTowards(transform.position, targetPosUp, vibrationFrequency * currentRPM * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, targetPosUp, vibrationFrequency * targetRPM * Time.deltaTime);
                 
                 Debug.Log("Moving UP");
             }
@@ -152,7 +176,7 @@ public class DrillsRotationSimulation : MonoBehaviour
             if(transform.position.y > targetPosDown.y){
                 targetPosDown.x = transform.position.x;
                 targetPosDown.z = transform.position.z;
-                transform.position = Vector3.MoveTowards(transform.position, targetPosDown, vibrationFrequency * currentRPM * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(transform.position, targetPosDown, vibrationFrequency * targetRPM * Time.deltaTime);
 
                 Debug.Log("Moving DOWN");
             }
@@ -181,7 +205,9 @@ public class DrillsRotationSimulation : MonoBehaviour
             vibrationAmplitude = defaultVibrationAmplitude * torkbusterVibeAndFreqReductionFactor;
             vibrationFrequency = defaultVibrationFrequency * torkbusterVibeAndFreqReductionFactor;
 
-            drillToBeReplaced.gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
+            foreach(MeshRenderer mesh in drillToBeReplaced.gameObject.GetComponentsInChildren<MeshRenderer>()){
+                mesh.enabled = false;    
+            }
             torkBuster.gameObject.SetActive(true);
             isStuttering = false;
             UpdateUI();
@@ -189,7 +215,9 @@ public class DrillsRotationSimulation : MonoBehaviour
         else{
             vibrationAmplitude = defaultVibrationAmplitude;
             vibrationFrequency = defaultVibrationFrequency;
-            drillToBeReplaced.gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
+            foreach(MeshRenderer mesh in drillToBeReplaced.gameObject.GetComponentsInChildren<MeshRenderer>()){
+                mesh.enabled = true;    
+            }
             torkBuster.gameObject.SetActive(false);
             UpdateUI();
         }
@@ -197,38 +225,76 @@ public class DrillsRotationSimulation : MonoBehaviour
     }
 
     public void OnButtonIncreaseRPMPressed(){
+        if(isShocking) return;
+
         IncreaseRPM();
     }
 
     public void OnButtonDecreaseRPMPressed(){
+        if(isShocking) return;
+
         DecreaseRPM();
     }
 
     private void IncreaseRPM(){
-        if(currentRPM < maxRPM){
-            currentRPM += rpmIncreaseStep;
+        if(targetRPM < maxRPM){
+            targetRPM += rpmIncreaseStep;
+            
+            if(changeRPMCoro != null){
+                StopCoroutine(changeRPMCoro);
+            }
+            
+            changeRPMCoro = StartCoroutine(SlowlyIncreaseRPM());
+        }
+    }
+
+    private IEnumerator SlowlyIncreaseRPM()
+    {
+        // oldRPM = currentRPM;
+        while(currentRPM < targetRPM){
+            currentRPM += rpmIncreaseStep * rpmChangeSpeed * Time.deltaTime;
+            currentTorque = currentRPM * rpmToTorqueFactor;
             Mathf.Clamp(currentRPM, minRPM, maxRPM);
             zRotationEulersPerSecond = 360f * currentRPM / 60f;
-            oldRPM = currentRPM;
+            oldTorque = currentTorque;
+
             UpdateUI();
+            yield return null;
         }
     }
 
     private void DecreaseRPM(){
-        if(currentRPM > minRPM){
-            currentRPM -= rpmIncreaseStep;
-            Mathf.Clamp(currentRPM, minRPM, maxRPM);
-            zRotationEulersPerSecond = 360f * currentRPM / 60f;
-            oldRPM = currentRPM;
-            UpdateUI();
+        if(targetRPM > minRPM){
+            targetRPM -= rpmIncreaseStep;
+
+            if(changeRPMCoro != null){
+                StopCoroutine(changeRPMCoro);
+            }
+            
+            changeRPMCoro = StartCoroutine(SlowlyDecreaseRPM());
         }
     }
 
-    private void IncreaseRPMAndVibeDueToShock(){
-        if(currentRPM < maxRPM){
-            currentRPM += rpmIncreaseWithShock;
-            currentRPM = Mathf.Clamp(currentRPM, minRPM, maxRPM);
+    private IEnumerator SlowlyDecreaseRPM()
+    {
+        // oldRPM = currentRPM;
+        while(currentRPM > targetRPM){
+            currentRPM -= rpmIncreaseStep * rpmChangeSpeed * Time.deltaTime;
+            currentTorque = currentRPM * rpmToTorqueFactor;
+            Mathf.Clamp(currentRPM, minRPM, maxRPM);
             zRotationEulersPerSecond = 360f * currentRPM / 60f;
+            oldTorque = currentTorque;
+
+            UpdateUI();
+            yield return null;
+        }
+    }
+
+    private void IncreaseTorqueAndVibeDueToShock(){
+        if(currentTorque < maxRPM * rpmToTorqueFactor){
+            currentTorque += torqueIncreaseWithShock;
+            // currentRPM = Mathf.Clamp(currentRPM, minRPM, maxRPM);
+            // zRotationEulersPerSecond = 360f * currentTorque / 60f;
 
             if(isTorkbusterInstalled){
                 vibrationFrequency *= vibrationShockFactor;
@@ -252,13 +318,13 @@ public class DrillsRotationSimulation : MonoBehaviour
             if(shockCoRo != null){
                 StopCoroutine(shockCoRo);
             }
-            shockCoRo = StartCoroutine(RPMAndVibeReductionAfterSeconds());
+            shockCoRo = StartCoroutine(TorqueAndVibeReductionAfterSeconds());
         }
     }
 
-    IEnumerator RPMAndVibeReductionAfterSeconds(){
+    IEnumerator TorqueAndVibeReductionAfterSeconds(){
         float startTime = Time.time;
-        float deltaRPM = currentRPM - oldRPM;
+        float deltaTorque = currentTorque - oldTorque;
         
         float deltaVibeFreq, deltaVibeAmp;
         if(isTorkbusterInstalled){
@@ -270,17 +336,17 @@ public class DrillsRotationSimulation : MonoBehaviour
             deltaVibeAmp = vibrationAmplitude - defaultVibrationAmplitude;
         }
 
-        while(Time.time < startTime + rpmReductionDuration){
-            currentRPM -= deltaRPM / rpmReductionDuration * Time.deltaTime;
-            zRotationEulersPerSecond = 360f * currentRPM / 60f;
-            vibrationFrequency -= deltaVibeFreq / rpmReductionDuration * Time.deltaTime;
-            vibrationAmplitude -= deltaVibeAmp / rpmReductionDuration * Time.deltaTime;
+        while(Time.time < startTime + torqueReductionDuration){
+            currentTorque -= deltaTorque / torqueReductionDuration * Time.deltaTime;
+            // zRotationEulersPerSecond = 360f * currentTorque / 60f;
+            vibrationFrequency -= deltaVibeFreq / torqueReductionDuration * Time.deltaTime;
+            vibrationAmplitude -= deltaVibeAmp / torqueReductionDuration * Time.deltaTime;
             
             UpdateUI();
             yield return null;
         }
-        currentRPM = oldRPM;
-        zRotationEulersPerSecond = 360f * currentRPM / 60f;
+        currentTorque = oldTorque;
+        // zRotationEulersPerSecond = 360f * currentTorque / 60f;
 
         if(isTorkbusterInstalled){
             vibrationFrequency = defaultVibrationFrequency * torkbusterVibeAndFreqReductionFactor;
@@ -295,19 +361,94 @@ public class DrillsRotationSimulation : MonoBehaviour
     }
 
     public void OnButtonShockPressed(){
-        
-        IncreaseRPMAndVibeDueToShock();
+
+        GameObject particleGO;
+        bool isRed = false;
+        float timeToLive = 5f;
 
         if(isTorkbusterInstalled){
-            var particle = Instantiate(particlesBlue);
-            particle.Play();
-            Destroy(particle, 15f);
+            particleGO = blueParticlesPool.GetAPooledObject();
+            isRed = false;
+            if(goodShockFeedback != null)
+            {
+                StopCoroutine(goodShockFeedback);
+            }
+            goodShockFeedback = StartCoroutine(ShowGoodShockFeedFlash());
         }
         else{
-            var particle = Instantiate(particlesRed);
-            particle.Play();
-            Destroy(particle, 15f);
+            particleGO = redParticlesPool.GetAPooledObject();
+            isRed = true;
+            if(badShockFeedback != null)
+            {
+                StopCoroutine(badShockFeedback);
+            }
+            badShockFeedback = StartCoroutine(ShowBadShockFeedFlash());
         }
+
+        particleGO.GetComponent<ParticleSystem>().Play();
+        StartCoroutine(ReturnParticleSystemToPoolAfterSeconds(particleGO, isRed, timeToLive));
+
+        IncreaseTorqueAndVibeDueToShock();        
+    }
+
+    private IEnumerator ReturnParticleSystemToPoolAfterSeconds(GameObject go, bool isRed, float timeToLive)
+    {
+        yield return new WaitForSeconds(timeToLive);
+        if(isRed){
+            redParticlesPool.ReturnUsedObject(go);            
+        }
+        else{
+            blueParticlesPool.ReturnUsedObject(go);
+        }
+
+    }
+
+    private IEnumerator ShowBadShockFeedFlash()
+    {
+        shockFeedbackImageBad.enabled = true;
+
+        float fadeInMultiplier = 0.25f;
+        float fadeOutMultiplier = 0.5f;
+
+        yield return new WaitForSeconds(0.25f);
+        float startTime = Time.time;
+        while(Time.time < startTime + shockFeedbackDuration * fadeInMultiplier)
+        {
+            shockFeedbackImageBad.CrossFadeAlpha(1, shockFeedbackDuration *fadeInMultiplier, true);
+            yield return null;
+        }
+        
+        startTime = Time.time;
+        while(Time.time < startTime + shockFeedbackDuration * fadeOutMultiplier)
+        {
+            shockFeedbackImageBad.CrossFadeAlpha(0, shockFeedbackDuration * fadeOutMultiplier, true);
+            yield return null;
+        }
+        shockFeedbackImageBad.enabled = false;
+    }
+    private IEnumerator ShowGoodShockFeedFlash()
+    {
+        shockFeedbackImageGood.enabled = true;
+        // shockFeedbackImageGood.CrossFadeAlpha(0,0, true);
+
+        float fadeInMultiplier = 0.25f;
+        float fadeOutMultiplier = 0.5f;
+
+        yield return new WaitForSeconds(0.25f);
+        float startTime = Time.time;
+        while(Time.time < startTime + shockFeedbackDuration * fadeInMultiplier)
+        {
+            shockFeedbackImageGood.CrossFadeAlpha(1, shockFeedbackDuration * fadeInMultiplier, true);
+            yield return null;
+        }
+        
+        startTime = Time.time;
+        while(Time.time < startTime + shockFeedbackDuration * fadeOutMultiplier)
+        {
+            shockFeedbackImageGood.CrossFadeAlpha(0, shockFeedbackDuration * fadeOutMultiplier, true);
+            yield return null;
+        }
+        shockFeedbackImageGood.enabled = false;
     }
 
     IEnumerator RemoveTorkBuster(){
@@ -317,16 +458,20 @@ public class DrillsRotationSimulation : MonoBehaviour
         yield return null;
     }
 
-    private void AnimateRPMGaugeArrow(){
-        var defaultRotationZ = currentRPM * rpmToGaugeArrowfactor;
-        
-        rpmGaugeArrow.localRotation = Quaternion.Euler(0f,0f, currentRPM * rpmToGaugeArrowfactor + Random.Range(-currentRPM/100f, currentRPM/100f));
+    private void UpdateTorqueGauge(){
+        // var defaultRotationZ = currentRPM * rpmToGaugeArrowRotationfactor;
+        torqueGaugeArrow.localRotation = Quaternion.Euler(0f,0f, currentTorque * rpmToGaugeArrowRotationfactor + Random.Range(-currentTorque/100f, currentTorque/100f));
+        torqueGauge.color = minMaxGaugeColor.Evaluate(currentRPM / maxRPM);
     }
 
     private void UpdateUI(){
         textRPM.text = currentRPM.ToString($"000") + " RPM";
         textStutter.color = isStuttering ? stutterColor : Color.gray;
-        rpmSlider.value = currentRPM;
+
+        rpmUp.interactable = !isShocking;
+        rpmDown.interactable = !isShocking;
+
+        // rpmSlider.value = currentRPM;
 
         // rpmGaugeArrow.localRotation = Quaternion.Euler(0f,0f, currentRPM * rpmToGaugeArrowfactor);
     }
